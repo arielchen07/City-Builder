981932c17735d1b3c5af68748972086a1b05ca86
PyroPoro <rickywen12345@gmail.com>
2023-11-08 18:54:00 -0500

    Added buttons for road and util
    Added buttons on the right for roads and utility buildings, added the submenu and functionality

--- Unity/CityBuilder/Assets/Scripts/UI/MenuManager.cs
+++ Unity/CityBuilder/Assets/Scripts/UI/MenuManager.cs
@@ -6,11 +6,14 @@ public class MenuManager : MonoBehaviour
 {
     public GameObject inventory;
     public GameObject rightMenu;
+    public GameObject utilSubMenu;
     bool inventoryIsOpen = false;
+    bool isSubMenuOpen = false;
+    bool isRightMenuOpen = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -20,9 +23,28 @@ public class MenuManager : MonoBehaviour
     }
     public void ToggleInventory(){
         inventoryIsOpen = !inventoryIsOpen;
+        isRightMenuOpen = !isRightMenuOpen;
         inventory.GetComponent<Animator>().SetTrigger("toggle");
         inventory.GetComponent<Animator>().SetBool("isOpen", inventoryIsOpen);
         rightMenu.GetComponent<Animator>().SetTrigger("toggle");
-        rightMenu.GetComponent<Animator>().SetBool("isOpen", !inventoryIsOpen);
+        rightMenu.GetComponent<Animator>().SetBool("isOpen", isRightMenuOpen);
+    }
+
+    public void ToggleSubMenu(GameObject subMenu){
+        isSubMenuOpen = !isSubMenuOpen;
+        isRightMenuOpen = !isRightMenuOpen;
+        subMenu.GetComponent<Animator>().SetTrigger("toggle");
+        subMenu.GetComponent<Animator>().SetBool("isOpen", isSubMenuOpen);
+        rightMenu.GetComponent<Animator>().SetTrigger("toggle");
+        rightMenu.GetComponent<Animator>().SetBool("isOpen", isRightMenuOpen);
+    }
+
+    public void ToggleInventoryFromSubMenu(GameObject subMenu){
+        inventoryIsOpen = !inventoryIsOpen;
+        isSubMenuOpen = !isSubMenuOpen;
+        inventory.GetComponent<Animator>().SetTrigger("toggle");
+        inventory.GetComponent<Animator>().SetBool("isOpen", inventoryIsOpen);
+        subMenu.GetComponent<Animator>().SetTrigger("toggle");
+        subMenu.GetComponent<Animator>().SetBool("isOpen", isSubMenuOpen);
     }
 }
